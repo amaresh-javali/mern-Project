98aa45e85c8f44daf0e07b272507caa94bea68ba
Rishav <risasnha1999@gmail.com>
2023-10-13 20:14:05 +0530

    Subscription Plan Controller Changes

--- app/controllers/subscriptionPlanController.js
+++ app/controllers/subscriptionPlanController.js
@@ -55,46 +55,53 @@ subscriptionCltr.showPlan = async (request, response)=>
 //Update existing subscription plans.
 subscriptionCltr.update = async(req, res) =>{
     try{
-        const { name, amount } = req.body
-        const id = req.params.id
-        // console.log(id)
+        const { name, amount } = req.body;
+        const id = req.params.id;
 
         const updateSubscriptionPlan = await SubscriptionPlan.findByIdAndUpdate(
             id,
             {name, amount},
             {new: true}
         )
-        // console.log(updateSubscriptionPlan)
-        if(updateSubscriptionPlan) {
-            res.status(200).json(updateSubscriptionPlan)
-            console.log(updateSubscriptionPlan)
-        }
-        res.status(401).json({e:'error occure while update plan'})
-        
-    }catch(e){
-        res.status(400).json({error: 'subscription plan not found'})
 
+        if(updateSubscriptionPlan) 
+        {
+            res.status(200).json(updateSubscriptionPlan)
+        }
+        else
+        {
+            res.status(401).json('Error while updating the plan!');
+        }
+    }
+    catch(e)
+    {
+        res.status(400).json('Error! Please try again later !');
     }
 }
 
 //Delete existing plans. 
-subscriptionCltr.delete = async (req, res) => {
+subscriptionCltr.delete = async (req, res) => 
+{
     try {
-        const id = req.params.id; // Retrieve subscriptionPlanId from URL parameters
-        // console.log(id)
-        const plan = await SubscriptionPlan.findByIdAndDelete(id);
-        console.log(plan)
-        // if (!deleteSubscriptionPlan) {
-        //     res.status(400).json({ error: 'Subscription plan not found' });
-        // } else {
-        //     res.status(200).json({ deleteSubscriptionPlan, message: 'Subscription plan deleted successfully' }); // 204 status for successful deletion
-        // }
-        if(!plan) {
-            res.status(404).json({e:'plan not found' });
-        } else {
-            res.status(200).json(plan)
+        const planId = req.params.id; // Retrieve subscriptionPlanId from URL parameters
+        const userId = req.user._id; 
+        
+        const creator = await Creator.findOne({userId: userId});
+        if(creator)
+        {
+            const plan = await SubscriptionPlan.findByIdAndDelete(planId);
+            if(plan)
+            {
+                res.json(plan)
+            }
+            else
+            {
+                res.status(400).json('Plan not Found !');
+            }
         }
-    } catch (e) {
+    } 
+    catch (e) 
+    {
         res.json(e.message)
     }
 };
